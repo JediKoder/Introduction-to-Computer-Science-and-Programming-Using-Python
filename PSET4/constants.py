c3bca8ae9eaa7e045bf014d9cd87332ecf66ae4c
Jedi Koder <JediKoder@gmail.com>
2017-03-18 22:46:56 -0700

    Add files via upload

---
+++ PSET4/constants.py
@@ -0,0 +1,10 @@
+ENTER_WORD = 'Enter word, or a "." to indicate that you are finished: '
+INVALID_WORD = 'Invalid word, please try again.'
+OUT_OF_LETTERS = '\nRun out of letters.'
+BYE = 'Goodbye!'
+PLAY_GAME = 'Enter n to deal a new hand, r to replay the last hand, ' \
+                   'or e to end game: '
+INVALID_COMMAND = 'Invalid command\n'
+NO_HAND_PLAYED_YET = 'You have not played a hand yet. Please play a new ' \
+                      'hand first!\n'
+PLAYER_PROMPT = 'Enter u to have yourself play, c to have the computer play: '
